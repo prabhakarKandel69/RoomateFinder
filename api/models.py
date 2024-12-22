00e919c657f23bc540a8bd6e7800a4c1ab757319
prabhakarKandel69 <pkandel6969@gmail.com>
2024-12-22 19:00:43 +0545

    Adding new preference fields

--- api/models.py
+++ api/models.py
@@ -11,7 +11,20 @@ class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
     age = models.PositiveIntegerField(null=True, blank=True)
     gender = models.CharField(max_length=1, choices=GENDER_CHOICES, null=True, blank=True)
+    address = models.CharField(max_length=255, null=True, blank=True)
+    smoking_allowed = models.BooleanField(null=True, blank=True)
+    pets_allowed = models.BooleanField(null=True, blank=True)
+    early_riser = models.BooleanField(null=True, blank=True)
+    vegeterian = models.BooleanField(null=True, blank=True)
+    gender_same_prefer = models.BooleanField(null=True, blank=True)
+    introvert = models.BooleanField(null=True, blank=True)
+    min_budget = models.PositiveIntegerField(null=True, blank=True)
+    max_budget = models.PositiveIntegerField(null=True, blank=True)
+
+
 
     def __str__(self):
         return f"{self.user.username}'s profile"
+    
+
 
