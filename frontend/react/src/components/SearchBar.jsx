aad05a5508b2992a18bc08de6646f7e8932111b5
= <=>
2025-01-14 10:02:05 +0545

    feat:searching Page modified
    new  UI is Added

--- frontend/react/src/components/SearchBar.jsx
+++ frontend/react/src/components/SearchBar.jsx
@@ -2,16 +2,22 @@ import React, { useState } from "react";
 
 const SearchBar = ({ onSearch }) => {
   const [searchTerm, setSearchTerm] = useState("");
+  const [roomType, setRoomType] = useState("have room"); // State for dropdown value
 
   const handleInputChange = (e) => {
     const value = e.target.value;
     setSearchTerm(value);
   };
 
+  const handleSelectChange = (e) => {
+    setRoomType(e.target.value); // Update the room type state when the dropdown value changes
+  };
+
   const handleSubmit = (e) => {
     e.preventDefault();
     if (onSearch) {
-      onSearch(searchTerm); // Trigger search logic
+      // Pass both search term and selected room type to onSearch function
+      onSearch({ searchTerm, roomType });
     }
   };
 
@@ -22,7 +28,13 @@ const SearchBar = ({ onSearch }) => {
     >
       {/* Input Field */}
       <div className="flex items-center flex-grow bg-[#E7F8FD] rounded-lg px-3 py-2">
-        <span className="text-[#243B55] text-lg mr-2"> <img src="../img/location.svg" alt="location" className="h-[24px] w-[24px] mr-1" /></span>
+        <span className="text-[#243B55] text-lg mr-2">
+          <img
+            src="../img/location.svg"
+            alt="location"
+            className="h-[24px] w-[24px] mr-1"
+          />
+        </span>
         <input
           type="text"
           className="flex-grow bg-transparent focus:outline-none text-[#243B55] text-sm"
@@ -42,15 +54,25 @@ const SearchBar = ({ onSearch }) => {
 
       {/* Dropdown */}
       <div className="relative bg-[#E7F8FD] px-3 py-2 rounded-lg flex items-center">
-        <span className="text-[#243B55] text-lg mr-2"><img src="../img/house.svg" alt="location" className="h-[24px] w-[24px] mr-1" /></span>
+        <span className="text-[#243B55] text-lg mr-2">
+          <img
+            src="../img/house.svg"
+            alt="house"
+            className="h-[24px] w-[24px] mr-1"
+          />
+        </span>
         <select
           className="bg-primary focus:outline-none text-secondary text-sm font-bold"
-          defaultValue="have room"
+          value={roomType}
+          onChange={handleSelectChange}
         >
-          <option value="have room" className="text-secondary font-bold">Looking for Roommate with Room</option>
-          <option value="no room" className="text-secondary font-bold">Looking for Roommate without Room</option>
+          <option value="have room" className="text-secondary font-bold">
+            Looking for Roommate with Room
+          </option>
+          <option value="no room" className="text-secondary font-bold">
+            Looking for Roommate without Room
+          </option>
         </select>
-
       </div>
     </form>
   );
