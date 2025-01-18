9e1b4fd43eb012fd13467ca442ec62eb36b907e8
prabhakarKandel69 <pkandel6969@gmail.com>
2025-01-18 10:51:38 +0545

    Seen at for messages model and ordering

--- chat/models.py
+++ chat/models.py
@@ -12,6 +12,10 @@ class Message(models.Model):
     message_text = models.TextField()
     attachment = models.ImageField(upload_to="user/attachments/",null=True,blank=True)
     seen = models.BooleanField(default=False)
+    seenat = models.DateTimeField(null=True,blank=True)
+
+    class Meta:
+        ordering = ["-sent_at"]
 
     def __str__(self):
         return f"{self.sender} message to {self.receiver}"
