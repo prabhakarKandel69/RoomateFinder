accfa642cf05589a067ee0f3fee23d409eae181e
prabhakarKandel69 <pkandel6969@gmail.com>
2025-02-02 09:11:08 +0545

    Prolonged token lifetime

--- RoomateFinder/settings.py
+++ RoomateFinder/settings.py
@@ -112,8 +112,8 @@ REST_FRAMEWORK = {
 from datetime import timedelta
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=30),
-    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=1),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=30),
     'ROTATE_REFRESH_TOKENS': True,
     'BLACKLIST_AFTER_ROTATION': True,
     'AUTH_HEADER_TYPES': ('Bearer',),
